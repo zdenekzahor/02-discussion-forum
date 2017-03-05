ac33849c9b4591e8f29f6d4baa575d76b5fcf511
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2017-03-05 22:47:46 +0100

    App skeleton with complete solution added

---
+++ src/components/AddPost.js
@@ -0,0 +1,14 @@
+import React, {PropTypes} from 'react';
+import FlatButton from 'material-ui/FlatButton';
+
+const AddPost = (props) => {
+  const {onTap} = props;
+
+  return (<FlatButton label="Add post" onTouchTap={onTap} />);
+};
+
+AddPost.propTypes = {
+  onTap: PropTypes.func.isRequired
+};
+
+export default AddPost;
