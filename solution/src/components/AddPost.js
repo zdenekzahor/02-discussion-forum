37862504c554d62f0cf930e285779e44bdedb089
Jaroslav Kubicek <jaroslav.kubicek@merck.com>
2017-03-08 20:21:41 +0100

    hotfixing my crappy untested component

--- solution/src/components/AddPost.js
+++ solution/src/components/AddPost.js
@@ -2,7 +2,9 @@ import React, {PropTypes} from "react";
 import FlatButton from "material-ui/FlatButton";
 
 const AddPost = (props) => {
-  const {onTap} = props;
+  const onTap = () => {
+    props.onTap(true);
+  };
 
   return (<FlatButton label="Add post" onTouchTap={onTap} />);
 };
